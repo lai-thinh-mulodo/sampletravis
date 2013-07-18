ed4c174775ed9536e70e28a1d81d1a0c8954c1a2
Lại Huy Thịnh <lai.huy.thinh@gmail.com>
2013-07-18 17:27:02 +0700

    uuuuu

--- src/test/java/com/laihuythinh/android/HelloAndroidActivityTest.java
+++ src/test/java/com/laihuythinh/android/HelloAndroidActivityTest.java
@@ -1,29 +1,12 @@
 package test.java.com.laihuythinh.android;
 
-import main.java.com.laihuythinh.android.HelloAndroidActivity;
-
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
 
-import com.xtremelabs.robolectric.RobolectricTestRunner;
-
-@RunWith(RobolectricTestRunner.class)
 public class HelloAndroidActivityTest {
 	
-	private HelloAndroidActivity activity;
-	
-	@Before
-    public void setUp() {
-        activity = new HelloAndroidActivity();
-        activity.onCreate(null);
-    }
-
 	@Test
-    public void itProperlyGreetsYou() {
-		String hello = "Hello World, MyActivity!";
-        assertThat(hello, equalTo("Hello World, MyActivity!"));
+    public void testHelloAndroidScreen() {
+		Assert.assertEquals(36, 36);
     }
 }
