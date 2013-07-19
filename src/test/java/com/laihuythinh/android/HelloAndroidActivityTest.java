559400e14a23b2a56b69d85878c7276731c6e29c
Lại Huy Thịnh <lai.huy.thinh@gmail.com>
2013-07-19 17:04:01 +0700

    Update

--- src/test/java/com/laihuythinh/android/HelloAndroidActivityTest.java
+++ src/test/java/com/laihuythinh/android/HelloAndroidActivityTest.java
@@ -10,10 +10,10 @@ public class HelloAndroidActivityTest {
 		Assert.assertEquals(36, 36);
 	}
 	
-	@Test
-	public void testHelloAndroidScreenNG() {
-		Assert.assertEquals(36, 50);
-	}
+//	@Test
+//	public void testHelloAndroidScreenNG() {
+//		Assert.assertEquals(36, 50);
+//	}
 	
 	@Test
 	public void testStringOK() {
@@ -21,9 +21,9 @@ public class HelloAndroidActivityTest {
 		Assert.assertEquals(myapp, "com.laihuythinh.android");
 	}
 	
-	@Test
-	public void testStringNG() {
-		String myapp = "com.laihuythinh.android";
-		Assert.assertEquals(myapp, "com.abc.android");
-	}
+//	@Test
+//	public void testStringNG() {
+//		String myapp = "com.laihuythinh.android";
+//		Assert.assertEquals(myapp, "com.abc.android");
+//	}
 }
